e599011e909c55ae3a03e65d5f805b40a06797d4
ArchanaSathiyaseelans <archanasathiyaseelans26@gmail.com>
2026-06-11 00:47:51 +0530

    Footer, Layout, Navbar, PageLoader, SentryErrorFallBack, and SentryUserSync. Integrated Sentry for error tracking and user content synchronization with clerk. -Updated main entry point to include OueryClientProvider and Sentry error Boundary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,34 @@
-import "./App.css";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import {
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useAuth,
+} from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+import Layout from "./components/Layout";
 
 function App() {
+  const { isLoaded } = useAuth();
+
+  if (!isLoaded) return <PageLoader />;
+
   return (
     <>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
+      <Layout>
+        <header>
+          <Show when="signed-out">
+            <SignInButton mode="modal" />
+            <SignUpButton mode="modal" />
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </header>
+
+        <p className="text-red-500">Hello</p>
+        <button className="btn btn-primary">Click me</button>
+      </Layout>
     </>
   );
 }
